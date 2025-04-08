25d3fb84b8179b531302f2123520456f2286d622
Todsapol Mangkalakeelee <ohoponza5@hotmail.com>
2025-04-08 22:23:21 +0700

    creat-pool-and-async

--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,41 @@
 import express from "express";
-
+import connectionPool from "./utils/db.mjs";
 const app = express();
-const port = 4001;
-
+const port = 3001;
+app.use(express.json());
 app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
+  return res.json("Server API is working ");
 });
-
+app.post("/assignments", async (req, res) => {
+  const newAssignment = {
+    ...req.body,
+    created_at: new Date(),
+    updated_at: new Date(),
+    published_at: new Date(),
+  };
+  try {
+    await connectionPool.query(
+      'insert into assignments (user_id, title, content, category, length, created_at, updated_at, published_at, status) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      [
+        newAssignment.user_id,
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.length,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at,
+        newAssignment.status,
+      ]
+    );
+  } catch (error) {
+    console.error("Database error:", error);
+    return res.status(500).json({
+      message: "Server could not create assignment because of database connection",
+    });
+  }
+  return res.status(201).json({message: "Created assignment successfully"});
+});
 app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -1,10 +1,10 @@
 // Create PostgreSQL Connection Pool here !
-import * as pg from "pg";
-const { Pool } = pg.default;
+import pg from "pg";
+const { Pool } = pg;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:P@ssw0rd@localhost:5432/posts",
 });
 
 export default connectionPool;
